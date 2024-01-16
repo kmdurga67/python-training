c390281e01e2892191bd82b104ce67e68d6c3445
Kumari Durga <singhdurga5698@gmail.com>
2024-01-16 13:10:09 +0530

    added code for command line inputs

--- main.py
+++ main.py
@@ -1,48 +1,34 @@
+from calculator.operations import add, subtract, multiply, divide
 
-import sys
-
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y != 0:
-        return x / y
+def perform_operation(num1, operator, num2):
+    if operator == 'add':
+        return add(num1, num2)
+    elif operator == 'sub':
+        return subtract(num1, num2)
+    elif operator == 'mult':
+        return multiply(num1, num2)
+    elif operator == 'div':
+        return divide(num1, num2)
     else:
-        return "Cannot divide by zero"
+        return "Invalid operator. Please use add, sub, mult, div"
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python main.py <number1> <operator> <number2>")
-        sys.exit(1)
+    while True:
+        input_str = input("Enter values like <number1> <operator> <number2> (or type 'exit' to end the program): ")
 
-    try:
-        num1 = float(sys.argv[1])
-        operator = sys.argv[2]
-        num2 = float(sys.argv[3])
+        if input_str.lower() == 'exit':
+            break
 
-        if operator == '+':
-            result = add(num1, num2)
-        elif operator == '-':
-            result = subtract(num1, num2)
-        elif operator == '*':
-            result = multiply(num1, num2)
-        elif operator == '/':
-            result = divide(num1, num2)
-        else:
-            print("Invalid operator. Please use +, -, *, /")
-            sys.exit(1)
+        values = input_str.split()
+        if len(values) != 3:
+            print("Invalid input. Please enter values like <number1> <operator> <number2>.")
+            continue
 
-        print(f"Result: = {result}")
-
-    except ValueError:
-        print("Invalid input. Please enter valid numbers.")
-        sys.exit(1)
+        num1 = float(values[0])
+        operator = values[1]
+        num2 = float(values[2])
+        result = perform_operation(num1, operator, num2)
+        print(result)
 
 if __name__ == "__main__":
     main()
