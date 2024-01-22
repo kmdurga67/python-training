8eca083f71c80fb8793d8ad9c6a64a6cc9b66ae0
Kumari Durga <singhdurga5698@gmail.com>
2024-01-22 10:52:45 +0530

    added cli_calculator

--- cli_calculator/main.py
+++ cli_calculator/main.py
@@ -1,5 +1,6 @@
 from calculator.operations import add, subtract, multiply, divide
 
+
 def perform_operation(num1, operator, num2):
     if operator == 'add':
         return add(num1, num2)
@@ -12,16 +13,19 @@ def perform_operation(num1, operator, num2):
     else:
         return "Invalid operator. Please use add, sub, mult, div"
 
+
 def main():
     while True:
-        input_str = input("Enter values like <number1> <operator> <number2> (or type 'exit' to end the program): ")
+        input_str = input(
+            "Enter values like <number1> <operator> <number2> (or type 'exit' to end the program): ")
 
         if input_str.lower() == 'exit':
             break
 
         values = input_str.split()
         if len(values) != 3:
-            print("Invalid input. Please enter values like <number1> <operator> <number2>.")
+            print(
+                "Invalid input. Please enter values like <number1> <operator> <number2>.")
             continue
 
         num1 = float(values[0])
@@ -29,6 +33,7 @@ def main():
         num2 = float(values[2])
         result = perform_operation(num1, operator, num2)
         print(result)
+
 
 if __name__ == "__main__":
     main()
